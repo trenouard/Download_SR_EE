1aacf2a4422dd1dc150e28f2243032c25169da51
trenouard <theo.renouard@hotmail.com>
2022-06-17 21:10:53 +0200

    Add download python Scripts

---
+++ SDS_process.py
@@ -0,0 +1,275 @@
+# load modules
+import os
+import pdb
+import glob
+import numpy as np
+import xarray as xr
+import pandas as pd
+from tqdm import tqdm
+from osgeo import gdal, osr
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+# image processing modules
+import skimage.filters as filters
+import skimage.measure as measure
+import skimage.morphology as morphology
+
+# other modules
+import pickle
+from pylab import ginput
+import matplotlib.cm as cm
+from datetime import date
+from datetime import datetime
+from matplotlib import gridspec
+import matplotlib.lines as mlines
+import matplotlib.patches as mpatches
+
+
+# Modules
+import SDS_tools, SDS_preprocess
+
+np.seterr(all='ignore') # raise/ignore divisions by 0 and nans
+
+
+def get_ndvi_lst(metadata, settings):
+    
+    """
+    Reads the metadata of all Landsat and Sentinel Surface Reflectance images to compute the NDVI from the multispectral     bands and save the NDVI and LST as tiff and zarr files
+
+    Arguments:
+    -----------
+    metadata: dict
+        contains all the information about the satellite images that were downloaded
+    settings: dict with the following keys
+        'inputs': dict
+                'sitename': str
+                name of the site
+                'polygon': list
+                    polygon containing the lon/lat coordinates to be extracted,
+                    longitudes in the first column and latitudes in the second column,
+                    there are 5 pairs of lat/lon with the fifth point equal to the first point:
+                    ```
+                    polygon = [[[151.3, -33.7],[151.4, -33.7],[151.4, -33.8],[151.3, -33.8],
+                    [151.3, -33.7]]]
+                    ```
+                'dates': list of str
+                    list that contains 2 strings with the initial and final dates in 
+                    format 'yyyy-mm-dd':
+                    ```
+                    dates = ['1987-01-01', '2018-01-01']
+                    ```
+                'sat_list': list of str
+                    list that contains the names of the satellite missions to include: 
+                    ```
+                    sat_list = ['L5', 'L7', 'L8', 'S2']
+                    ```
+                'filepath_data': str
+                    filepath to the directory where the images are downloaded
+                    
+        'cloud_thresh': float
+            value between 0 and 1 indicating the maximum cloud fraction in 
+            the cropped image that is accepted
+        'cloud_mask_issue': boolean
+            True if there is an issue with the cloud mask and sand pixels
+            are erroneously being masked on the images
+        
+
+    Returns:
+    -----------
+    Save NDVI and LST as tif and zarr files
+
+    """
+    
+    sitename = settings['inputs']['sitename']
+    filepath_data = settings['inputs']['filepath']
+    
+    # loop through satellite list
+    for satname in metadata.keys():
+        
+        # create a subfolder to store the NDVI images
+        filepath_ndvi = os.path.join(filepath_data, sitename, satname, 'NDVI')
+        if not os.path.exists(filepath_ndvi):
+            os.makedirs(filepath_ndvi)
+            
+        if satname in ['L5', 'L7', 'L8']:
+            # create a subfolder to store the LST images
+            filepath_lst = os.path.join(filepath_data, sitename, satname, 'LST')
+            if not os.path.exists(filepath_lst):
+                os.makedirs(filepath_lst)
+
+        # get images
+        filepath = SDS_tools.get_filepath(settings['inputs'],satname)       
+        filenames = metadata[satname]['filenames']
+        
+        print('')
+        
+         # loop through the images
+        for i in range(len(filenames)):
+
+            print('\r%s:   %d%%' % (satname,int(((i+1)/len(filenames))*100)), end='')
+
+            # get image filename
+            fn = SDS_tools.get_filenames(filenames[i],filepath, satname)
+            # preprocess image (cloud mask + pansharpening/downsampling)
+            im_ms, georef, cloud_mask, im_extra, im_QA, im_nodata = SDS_preprocess.preprocess_single(fn, satname, settings['cloud_mask_issue'])
+            # get image spatial reference system (epsg code) from metadata dict
+            image_epsg = metadata[satname]['epsg'][i]
+            
+            # compute cloud_cover percentage (with no data pixels)
+            cloud_cover_combined = np.divide(sum(sum(cloud_mask.astype(int))),
+                                    (cloud_mask.shape[0]*cloud_mask.shape[1]))
+            if cloud_cover_combined > 0.99: # if 99% of cloudy pixels in image skip
+                continue
+            # remove no data pixels from the cloud mask 
+            # (for example L7 bands of no data should not be accounted for)
+            cloud_mask_adv = np.logical_xor(cloud_mask, im_nodata) 
+            # compute updated cloud cover percentage (without no data pixels)
+            cloud_cover = np.divide(sum(sum(cloud_mask_adv.astype(int))),
+                                    (sum(sum((~im_nodata).astype(int)))))
+            # skip image if cloud cover is above user-defined threshold
+            if cloud_cover > settings['cloud_thresh']:
+                continue
+            
+            # Compute and save NDVI and LST 
+            dataset = gdal.Open(fn)
+            
+            # Compute NDVI                
+            ndvi = SDS_tools.nd_index(im_ms[:,:,3], im_ms[:,:,2], cloud_mask)
+
+            #Get projection
+            prj = dataset.GetProjection()
+            geotransform = dataset.GetGeoTransform()
+            
+            output_file_ndvi = os.path.join(filepath_ndvi,filenames[i])
+            
+            # Create gtif file with rows and columns from parent raster 
+            band = dataset.GetRasterBand(1)
+            driver = gdal.GetDriverByName("GTiff")
+            columns, rows = (band.XSize, band.YSize)
+            
+            ndvi_ds = driver.Create(output_file_ndvi, 
+                                   columns, 
+                                   rows, 
+                                   1, 
+                                   gdal.GDT_Int32)
+
+            ##writting output raster
+            ndvi_ds.GetRasterBand(1).WriteArray(ndvi*10000)
+
+            #setting extension of output raster
+            # top left x, w-e pixel resolution, rotation, top left y, rotation, n-s pixel resolution
+            
+            ndvi_ds.SetGeoTransform(geotransform)
+
+            # setting spatial reference of output raster 
+            srs = osr.SpatialReference(wkt = prj)
+            ndvi_ds.SetProjection( srs.ExportToWkt() )
+            
+            #Close output raster dataset 
+            ndvi_ds = None
+            
+            # Save LST for Landsat
+            if satname in ['L5', 'L7', 'L8']:
+                lst = im_ms[:,:,5]
+                output_file_lst = os.path.join(filepath_lst,filenames[i])
+                lst_ds = driver.Create(output_file_lst, 
+                                   columns, 
+                                   rows, 
+                                   1, 
+                                   band.DataType)
+                lst_ds.GetRasterBand(1).WriteArray( lst )
+                lst_ds.SetGeoTransform(geotransform)
+                lst_ds.SetProjection( srs.ExportToWkt() )
+                lst_ds = None
+
+            #Close main raster dataset
+            dataset = None
+            
+            
+        ### Save every datasets as zarr files ###
+        
+        print('\nSaving NDVI as Zarr')
+        # filepath to store the NDVI zarr file
+        filepath_ndvi_zarr = os.path.join(filepath_data, sitename, satname, 'NDVI_zarr')                
+
+        # Get list of NDVI .tif files
+        files = glob.glob(filepath_ndvi + "/*.tif")
+
+        # Create Xarray dataset
+        da = create_xarray(files)
+        
+        #Convert Floats to Integers
+        da['band'] = da.band.astype('int16')
+        
+        # Save as a zarr file
+        da.to_zarr(filepath_ndvi_zarr, mode='a')
+         
+        # Do the same for LST
+        if satname in ['L5', 'L7', 'L8']:
+            print('\nSaving LST as Zarr')
+            # filepath to store the LST zarr file
+            filepath_lst_zarr = os.path.join(filepath_data, sitename, satname, 'LST_zarr')
+            
+            # Get list of NDVI .tif files
+            files = glob.glob(filepath_lst + "/*.tif")
+
+            # Create Xarray dataset
+            da = create_xarray(files)
+
+            #Convert Floats to Integers
+            da['band'] = da.band.astype('int16')
+
+            # Save as a zarr file
+            da.to_zarr(filepath_lst_zarr, mode='a')
+
+
+            
+def filename_to_date(x: str):
+    '''
+    Converts file name to a gregorian date 
+    '''  
+    x = os.path.basename(x)
+    # getting year, month and dekad
+    year = int(x[:4])
+    month = int(x[5:7])
+    day = int(x[8:10])
+    
+    # Converting to gregorian date
+    greg_date = date(year,month,day)
+        
+    return greg_date
+
+
+def create_xarray(files: list):
+    '''
+    Concatenate all rasters (tiff) into an Xarray
+    '''
+    
+    # Download first raster
+    da = xr.open_rasterio(files[0])
+    
+    # Concatenate all rasters along time
+    for file in tqdm(files[1:]):
+        raster = xr.open_rasterio(file)
+        da = xr.concat([da, raster], dim = 'time')
+    
+    # Get the timestamps from the filenames
+    timestamps = [
+            filename_to_date(file)
+            for file in files
+        ]
+
+    # Assign them to the time dimension:
+    da = da.assign_coords(time=pd.to_datetime(timestamps))
+    
+    # Remove band dimension
+    da = da.squeeze('band')
+    da = da.drop_vars('band')
+    da = da.rename({'y':'latitude', 'x':'longitude', 'time':'time'})
+    
+    return da.to_dataset(name='band')
+       
+
+            
+        
---
+++ SDS_tools.py
@@ -0,0 +1,454 @@
+# load modules
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+
+# other modules
+from osgeo import gdal, osr
+import geopandas as gpd
+from shapely import geometry
+import skimage.transform as transform
+from astropy.convolution import convolve
+
+np.seterr(all='ignore') # raise/ignore divisions by 0 and nans
+
+###################################################################################################
+# COORDINATES CONVERSION FUNCTIONS
+###################################################################################################
+
+def convert_pix2world(points, georef):
+    """
+    Converts pixel coordinates (pixel row and column) to world projected 
+    coordinates performing an affine transformation.
+
+    Arguments:
+    -----------
+    points: np.array or list of np.array
+        array with 2 columns (row first and column second)
+    georef: np.array
+        vector of 6 elements [Xtr, Xscale, Xshear, Ytr, Yshear, Yscale]
+                
+    Returns:    
+    -----------
+    points_converted: np.array or list of np.array 
+        converted coordinates, first columns with X and second column with Y
+        
+    """
+    
+    # make affine transformation matrix
+    aff_mat = np.array([[georef[1], georef[2], georef[0]],
+                       [georef[4], georef[5], georef[3]],
+                       [0, 0, 1]])
+    # create affine transformation
+    tform = transform.AffineTransform(aff_mat)
+
+    # if list of arrays
+    if type(points) is list:
+        points_converted = []
+        # iterate over the list
+        for i, arr in enumerate(points): 
+            tmp = arr[:,[1,0]]
+            points_converted.append(tform(tmp))
+          
+    # if single array
+    elif type(points) is np.ndarray:
+        tmp = points[:,[1,0]]
+        points_converted = tform(tmp)
+        
+    else:
+        raise Exception('invalid input type')
+        
+    return points_converted
+
+def convert_world2pix(points, georef):
+    """
+    Converts world projected coordinates (X,Y) to image coordinates 
+    (pixel row and column) performing an affine transformation.
+
+    Arguments:
+    -----------
+    points: np.array or list of np.array
+        array with 2 columns (X,Y)
+    georef: np.array
+        vector of 6 elements [Xtr, Xscale, Xshear, Ytr, Yshear, Yscale]
+                
+    Returns:    
+    -----------
+    points_converted: np.array or list of np.array 
+        converted coordinates (pixel row and column)
+    
+    """
+    
+    # make affine transformation matrix
+    aff_mat = np.array([[georef[1], georef[2], georef[0]],
+                       [georef[4], georef[5], georef[3]],
+                       [0, 0, 1]])
+    # create affine transformation
+    tform = transform.AffineTransform(aff_mat)
+    
+    # if list of arrays
+    if type(points) is list:
+        points_converted = []
+        # iterate over the list
+        for i, arr in enumerate(points): 
+            points_converted.append(tform.inverse(points))
+            
+    # if single array    
+    elif type(points) is np.ndarray:
+        points_converted = tform.inverse(points)
+        
+    else:
+        print('invalid input type')
+        raise
+        
+    return points_converted
+
+
+def convert_epsg(points, epsg_in, epsg_out):
+    """
+    Converts from one spatial reference to another using the epsg codes
+    
+    Arguments:
+    -----------
+    points: np.array or list of np.ndarray
+        array with 2 columns (rows first and columns second)
+    epsg_in: int
+        epsg code of the spatial reference in which the input is
+    epsg_out: int
+        epsg code of the spatial reference in which the output will be            
+                
+    Returns:    
+    -----------
+    points_converted: np.array or list of np.array 
+        converted coordinates from epsg_in to epsg_out
+        
+    """
+    
+    # define input and output spatial references
+    inSpatialRef = osr.SpatialReference()
+    inSpatialRef.ImportFromEPSG(epsg_in)
+    outSpatialRef = osr.SpatialReference()
+    outSpatialRef.ImportFromEPSG(epsg_out)
+    # create a coordinates transform
+    coordTransform = osr.CoordinateTransformation(inSpatialRef, outSpatialRef)
+    # if list of arrays
+    if type(points) is list:
+        points_converted = []
+        # iterate over the list
+        for i, arr in enumerate(points): 
+            points_converted.append(np.array(coordTransform.TransformPoints(arr)))
+    # if single array
+    elif type(points) is np.ndarray:
+        points_converted = np.array(coordTransform.TransformPoints(points))  
+    else:
+        raise Exception('invalid input type')
+
+    return points_converted
+
+###################################################################################################
+# IMAGE ANALYSIS FUNCTIONS
+###################################################################################################
+    
+def nd_index(im1, im2, cloud_mask):
+    """
+    Computes normalised difference index on 2 images (2D), given a cloud mask (2D).
+
+    Arguments:
+    -----------
+    im1: np.array
+        first image (2D) with which to calculate the ND index
+    im2: np.array
+        second image (2D) with which to calculate the ND index
+    cloud_mask: np.array
+        2D cloud mask with True where cloud pixels are
+
+    Returns:    
+    -----------
+    im_nd: np.array
+        Image (2D) containing the ND index
+        
+    """
+
+    # reshape the cloud mask
+    vec_mask = cloud_mask.reshape(im1.shape[0] * im1.shape[1])
+    # initialise with NaNs
+    vec_nd = np.ones(len(vec_mask)) * np.nan
+    # reshape the two images
+    vec1 = im1.reshape(im1.shape[0] * im1.shape[1])
+    vec2 = im2.reshape(im2.shape[0] * im2.shape[1])
+    # compute the normalised difference index
+    temp = np.divide(vec1[~vec_mask] - vec2[~vec_mask],
+                     vec1[~vec_mask] + vec2[~vec_mask])
+    vec_nd[~vec_mask] = temp
+    # reshape into image
+    im_nd = vec_nd.reshape(im1.shape[0], im1.shape[1])
+
+    return im_nd
+    
+def image_std(image, radius):
+    """
+    Calculates the standard deviation of an image, using a moving window of 
+    specified radius. Uses astropy's convolution library'
+    
+    Arguments:
+    -----------
+    image: np.array
+        2D array containing the pixel intensities of a single-band image
+    radius: int
+        radius defining the moving window used to calculate the standard deviation. 
+        For example, radius = 1 will produce a 3x3 moving window.
+        
+    Returns:    
+    -----------
+    win_std: np.array
+        2D array containing the standard deviation of the image
+        
+    """  
+    
+    # convert to float
+    image = image.astype(float)
+    # first pad the image
+    image_padded = np.pad(image, radius, 'reflect')
+    # window size
+    win_rows, win_cols = radius*2 + 1, radius*2 + 1
+    # calculate std with uniform filters
+    win_mean = convolve(image_padded, np.ones((win_rows,win_cols)), boundary='extend',
+                        normalize_kernel=True, nan_treatment='interpolate', preserve_nan=True)
+    win_sqr_mean = convolve(image_padded**2, np.ones((win_rows,win_cols)), boundary='extend',
+                        normalize_kernel=True, nan_treatment='interpolate', preserve_nan=True)
+    win_var = win_sqr_mean - win_mean**2
+    win_std = np.sqrt(win_var)
+    # remove padding
+    win_std = win_std[radius:-radius, radius:-radius]
+
+    return win_std
+
+def mask_raster(fn, mask):
+    """
+    Masks a .tif raster using GDAL.
+    
+    Arguments:
+    -----------
+    fn: str
+        filepath + filename of the .tif raster
+    mask: np.array
+        array of boolean where True indicates the pixels that are to be masked
+        
+    Returns:    
+    -----------
+    Overwrites the .tif file directly
+        
+    """ 
+    
+    # open raster
+    raster = gdal.Open(fn, gdal.GA_Update)
+    # mask raster
+    for i in range(raster.RasterCount):
+        out_band = raster.GetRasterBand(i+1)
+        out_data = out_band.ReadAsArray()
+        out_band.SetNoDataValue(0)
+        no_data_value = out_band.GetNoDataValue()
+        out_data[mask] = no_data_value
+        out_band.WriteArray(out_data)
+    # close dataset and flush cache
+    raster = None
+
+
+###################################################################################################
+# UTILITIES
+###################################################################################################
+    
+def get_filepath(inputs,satname):
+    """
+    Create filepath to the different folders containing the satellite images.
+
+    Arguments:
+    -----------
+    inputs: dict with the following keys
+        'sitename': str
+            name of the site
+        'polygon': list
+            polygon containing the lon/lat coordinates to be extracted,
+            longitudes in the first column and latitudes in the second column,
+            there are 5 pairs of lat/lon with the fifth point equal to the first point:
+            ```
+            polygon = [[[151.3, -33.7],[151.4, -33.7],[151.4, -33.8],[151.3, -33.8],
+            [151.3, -33.7]]]
+            ```
+        'dates': list of str
+            list that contains 2 strings with the initial and final dates in 
+            format 'yyyy-mm-dd':
+            ```
+            dates = ['1987-01-01', '2018-01-01']
+            ```
+        'sat_list': list of str
+            list that contains the names of the satellite missions to include: 
+            ```
+            sat_list = ['L5', 'L7', 'L8', 'S2']
+            ```
+        'filepath_data': str
+            filepath to the directory where the images are downloaded
+    satname: str
+        short name of the satellite mission ('L5','L7','L8','S2')
+                
+    Returns:    
+    -----------
+    filepath: str or list of str
+        contains the filepath(s) to the folder(s) containing the satellite images
+    
+    """     
+    
+    sitename = inputs['sitename']
+    filepath_data = inputs['filepath']
+    # access the images
+    if satname == 'L5':
+        # access downloaded Landsat 5 images
+        filepath = os.path.join(filepath_data, sitename, satname, '30m')
+    elif satname == 'L7':
+        # access downloaded Landsat 7 images
+        filepath = os.path.join(filepath_data, sitename, 'L7', '30m')
+    elif satname == 'L8':
+        # access downloaded Landsat 8 images
+        filepath = os.path.join(filepath_data, sitename, 'L8', '30m')
+    elif satname == 'S2':
+        # access downloaded Sentinel 2 images
+        filepath10 = os.path.join(filepath_data, sitename, satname, '10m')
+        filepath20 = os.path.join(filepath_data, sitename, satname, '20m')
+        filepath60 = os.path.join(filepath_data, sitename, satname, '60m')
+        filepath = [filepath10, filepath20, filepath60]
+            
+    return filepath
+    
+def get_filenames(filename, filepath, satname):
+    """
+    Creates filepath + filename for all the bands belonging to the same image.
+
+    Arguments:
+    -----------
+    filename: str
+        name of the downloaded satellite image as found in the metadata
+    filepath: str or list of str
+        contains the filepath(s) to the folder(s) containing the satellite images
+    satname: str
+        short name of the satellite mission       
+        
+    Returns:    
+    -----------
+    fn: str or list of str
+        contains the filepath + filenames to access the satellite image
+        
+    """     
+    
+    if satname in ['L5', 'L7', 'L8']:
+        fn = os.path.join(filepath, filename)
+    if satname == 'S2':
+        filename20 = filename.replace('10m','20m')
+        filename60 = filename.replace('10m','60m')
+        fn = [os.path.join(filepath[0], filename),
+              os.path.join(filepath[1], filename20),
+              os.path.join(filepath[2], filename60)]
+        
+    return fn
+
+
+
+###################################################################################################
+# CONVERSIONS FROM DICT TO GEODATAFRAME AND READ/WRITE GEOJSON
+###################################################################################################
+    
+def polygon_from_kml(fn):
+    """
+    Extracts coordinates from a .kml file.
+
+    Arguments:
+    -----------
+    fn: str
+        filepath + filename of the kml file to be read          
+                
+    Returns:    
+    -----------
+    polygon: list
+        coordinates extracted from the .kml file
+        
+    """    
+    
+    # read .kml file
+    with open(fn) as kmlFile:
+        doc = kmlFile.read() 
+    # parse to find coordinates field
+    str1 = '<coordinates>'
+    str2 = '</coordinates>'
+    subdoc = doc[doc.find(str1)+len(str1):doc.find(str2)]
+    coordlist = subdoc.split('\n')
+    # read coordinates
+    polygon = []
+    for i in range(1,len(coordlist)-1):
+        polygon.append([float(coordlist[i].split(',')[0]), float(coordlist[i].split(',')[1])])
+        
+    return [polygon]
+
+
+def get_image_bounds(fn):
+    """
+    Returns a polygon with the bounds of the image in the .tif file
+
+    Arguments:
+    -----------
+    fn: str
+        path to the image (.tif file)         
+                
+    Returns:    
+    -----------
+    bounds_polygon: shapely.geometry.Polygon
+        polygon with the image bounds
+        
+    """
+    
+    # nested functions to get the extent 
+    # copied from https://gis.stackexchange.com/questions/57834/how-to-get-raster-corner-coordinates-using-python-gdal-bindings
+    def GetExtent(gt,cols,rows):
+        'Return list of corner coordinates from a geotransform'
+        ext=[]
+        xarr=[0,cols]
+        yarr=[0,rows]
+        for px in xarr:
+            for py in yarr:
+                x=gt[0]+(px*gt[1])+(py*gt[2])
+                y=gt[3]+(px*gt[4])+(py*gt[5])
+                ext.append([x,y])
+            yarr.reverse()
+        return ext
+    
+    # load .tif file and get bounds
+    data = gdal.Open(fn, gdal.GA_ReadOnly)
+    gt = data.GetGeoTransform()
+    cols = data.RasterXSize
+    rows = data.RasterYSize
+    ext = GetExtent(gt,cols,rows)
+    
+    return geometry.Polygon(ext)
+
+def smallest_rectangle(polygon):
+    """
+    Converts a polygon to the smallest rectangle polygon with sides parallel
+    to coordinate axes.
+
+    Arguments:
+    -----------
+    polygon: list of coordinates 
+        pair of coordinates for 5 vertices, in clockwise order,
+        first and last points must match     
+                
+    Returns:    
+    -----------
+    polygon: list of coordinates
+        smallest rectangle polygon
+        
+    """
+    
+    multipoints = geometry.Polygon(polygon[0])
+    polygon_geom = multipoints.envelope
+    coords_polygon = np.array(polygon_geom.exterior.coords)
+    polygon_rect = [[[_[0], _[1]] for _ in coords_polygon]]
+    return polygon_rect
